7902fb74e66393e9fe0bdeac2f7eaa393688fdf0
monst <jerry.ca.zhu@gmail.com>
2023-02-23 21:20:22 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace _2Constructor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(9, 115);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(136, 499);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Card Head Font Size";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 620);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 26);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 19);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "No File Selected";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 91);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 19);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Confirm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 641);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Cards Loaded:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 641);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 68);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(136, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 663);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(258, 702);
+            this.MinimumSize = new System.Drawing.Size(258, 702);
+            this.Name = "Form1";
+            this.Text = "2Constructor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Word = Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+
+namespace _2Constructor
+{
+    public partial class Form1 : Form
+    {
+        private int headFontSize;
+
+        private List<Card> cards = new List<Card>();
+
+        //private int cardAmount = 0;
+
+        private Word.Document oDoc1;
+        Word.Document oDoc2;
+        private int labelNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.openFileDialog1.Multiselect = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            headFontSize = Convert.ToInt32(numericUpDown1.Value);
+
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    label2.Text = Path.GetFileName(file);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int count = oDoc2.Sentences.Count;
+
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    int pos = cards[i].position;
+                    int iterate = 1;
+                    //Conditions might be slow
+                    while (oDoc2.Sentences[pos + iterate].Font.Size != headFontSize && pos + iterate + 1 < count)
+                    {
+                        iterate++;
+                    }
+                    object startLocation = oDoc2.Sentences[pos].Start;
+                    object endLocation = oDoc2.Sentences[(pos + iterate) - 1].End;
+                    cards[i].range = oDoc2.Range(ref startLocation, ref endLocation);
+
+
+                    cards[i].range.Copy();
+                    oDoc1.Range(oDoc1.Content.End - 1, oDoc1.Content.End - 1).Paste();
+                }
+            }
+            saveFileDialog1.Filter = "Word Document|*.docx";
+            saveFileDialog1.ShowDialog();
+            oDoc1.SaveAs2(saveFileDialog1.FileName);
+            oDoc1.Close();
+            
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Word.Application oWord = new Word.Application();
+            oDoc2 = oWord.Documents.Open(openFileDialog1.FileName, null, true);
+            int count = oDoc2.Sentences.Count;
+
+            for (int i = 1; i <= count; i++)
+            {
+                if (oDoc2.Sentences[i].Font.Size == headFontSize)
+                {
+                    /*  move somewhere else
+                    while (oDoc2.Sentences[i + iterate].Font.Size != headFontSize && i + iterate + 1 < count)
+                    {
+                        iterate++;
+                    }
+                    */
+
+                    /* move somewhere else
+                    object startLocation = oDoc2.Sentences[i].Start;
+                    object endLocation = oDoc2.Sentences[(i + iterate) - 1].End;
+
+                    */
+                    Card cd = new Card();
+                    //cd.range = oDoc2.Range(ref startLocation, ref endLocation);
+                    cd.position = i;
+                    cards.Add(cd);
+
+                    checkedListBox1.Items.Add(oDoc2.Sentences[i].Text);
+
+                    labelNum++;
+                    label4.Text = labelNum.ToString();
+
+                    
+
+                }
+            }
+            oDoc1 = oWord.Documents.Add();
+            oWord.Visible = true;
+
+
+
+
+        }
+        /*
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int time1 = 0;
+            int time2 = 0;
+
+            //TRY REPLACE FOR WITH FOREACH
+            var watch = Stopwatch.StartNew();
+
+            Word.Application oWord = new Word.Application();
+            Word.Document oDoc2 = oWord.Documents.Open(openFileDialog1.FileName, null, true);
+
+            int count = oDoc2.Sentences.Count;
+
+            for (int i = 1; i <= count; i++)
+            {
+                if (oDoc2.Sentences[i].Font.Size == headFontSize)
+                {
+                    var watch1 = Stopwatch.StartNew();
+                    int iterate = 1;
+
+                    
+                    while (oDoc2.Sentences[i + iterate].Font.Size != headFontSize && i + iterate + 1 < count)
+                    {
+                        iterate++;
+                    }
+
+                    watch1.Stop();
+                    time1 += (int)watch1.ElapsedMilliseconds;
+                    Console.WriteLine((watch1.ElapsedMilliseconds / 1000f).ToString() + " Seconds, Watch 1");
+
+                    
+                    object startLocation = oDoc2.Sentences[i].Start;
+                    object endLocation = oDoc2.Sentences[(i + iterate) - 1].End;
+
+                    Card cd = new Card();
+                    cd.range = oDoc2.Range(ref startLocation, ref endLocation);
+                    cards.Add(cd);
+
+                    checkedListBox1.Items.Add(oDoc2.Sentences[i].Text);
+
+                    labelNum++;
+                    label4.Text = labelNum.ToString();
+
+                    
+                }
+            }
+
+            watch.Stop();
+
+            Console.WriteLine("Watch0 time: " + (watch.ElapsedMilliseconds / 1000f).ToString() + " Watch1 time: " + (time1/1000f).ToString());
+            Console.WriteLine((watch.ElapsedMilliseconds/1000).ToString() + " Seconds, Watch 0");
+
+
+            label6.Text = ((float)watch.ElapsedMilliseconds/1000f).ToString();
+
+            oDoc1 = oWord.Documents.Add();
+            oWord.Visible = true;
+
+           
+
+
+        }
+        */
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void openFileDialog1_FileOk(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void saveFileDialog1_FileOk(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            headFontSize = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    internal class Card
+    {
+        public int position { get; set; }
+        public Word.Range range { get; set; }
+    }
+}
